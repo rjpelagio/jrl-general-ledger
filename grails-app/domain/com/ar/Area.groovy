1c570b34acac1fc254c1391a5d30b60963716f5d
delmoangelo <delmoangelo@santarosa.sti.edu>
2014-06-03 11:34:08 +0000

    gelo 6/3/14

--- grails-app/domain/com/ar/Area.groovy
+++ grails-app/domain/com/ar/Area.groovy
@@ -2,39 +2,19 @@ package com.ar
 
 class Area {
 
-<<<<<<< .mine
+
 	static constraints = {
 
-	code ()
-	description()
+	code (blank : false)
+	description(blank : false)
 
 	 }
 
 	 static hasMany = [customerArea:CustomerArea]
 
-=======
-	static constraints = {
-	code()
-	description()	
 
-	}
-	static hasMany = [customerArea:CustomerArea]
-
->>>>>>> .r136
 	String code
 	String description
-
-	
-<<<<<<< .mine
-	
-=======
-	
-	
-    
->>>>>>> .r136
-
-
-					 
-	
+		
 
 }
