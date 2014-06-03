7602d6d4da825831a34ae11cc1456a7d5ff1d0fc
delmoangelo <delmoangelo@santarosa.sti.edu>
2014-06-03 11:30:15 +0000

    gelo 6/3/14

--- grails-app/controllers/com/ar/CustomerController.groovy
+++ grails-app/controllers/com/ar/CustomerController.groovy
@@ -1,9 +1,13 @@
-package com.ar
+package com.app
+import com.ar.EmployeeData
 
-class CustomerController {
+class EmployeeController {
 
     static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
-    
+
+    def appSearchService
+    def appUtilityService
+
     def beforeInterceptor = [action:this.&auth]
 
     def auth() {
@@ -19,90 +23,182 @@ class CustomerController {
 
     def list = {
         params.max = Math.min(params.max ? params.int('max') : 10, 100)
-        [customerInstanceList: Customer.list(params), customerInstanceTotal: Customer.count()]
+        def employee = new Employee()
+        employee.properties = params.properties
+        def party = new Party()
+        party.properties = params.properties
+        
+        def offset = 0
+        def result = appSearchService.getEmployees(
+            params.name, params.firstName, params.middleName, params.lastName, params.tin, employee.department, employee.position
+        )
+
+        if(params.offset){
+            offset = Math.min(params.offset ? params.int('offset') : 0, result.size())
+        }
+        //Record Count - if sql query is used
+        def recordCount = offset + params.max
+        if(recordCount >= result.size()){
+            recordCount = result.size()
+        }
+        [employeeInstanceList: result.subList(offset, recordCount), employeeInstanceTotal: result.size(), employeeInstance: employee, partyInstance: party, recordCount: recordCount]
     }
 
     def create = {
-        def customerInstance = new Customer()
-        customerInstance.properties = params
-        return [customerInstance: customerInstance]
+        def employeeData = new EmployeeData()
+        return [employeeData: employeeData]
     }
 
-    def save = {
-        def customerInstance = new Customer(params)
-        if (customerInstance.save(flush: true)) {
-            flash.message = "${message(code: 'default.created.message', args: [message(code: 'customer.label', default: 'Customer'), customerInstance.id])}"
-            redirect(action: "show", id: customerInstance.id)
-        }
-        else {
-            render(view: "create", model: [customerInstance: customerInstance])
+    def save = { EmployeeData data ->
+        def employeeInstance = new Employee(params)
+        def partyInstance = new Party()
+        def personInstance = new Person(params)
+        def partyRole = new PartyRole(params)
+
+        bindData(data, params)
+        data.status = 'Active'
+        def empRole = AppRole.findByRoleCode('CUST')
+        data.roleId = empRole.id
+        data.fullName = data.firstName + ' ' + data.middleName + ' ' + data.lastName
+        if(data.validate()){
+
+            partyInstance.name = data.fullName
+            partyInstance.lastName = data.lastName
+            partyInstance.firstName = data.firstName
+            partyInstance.middleName = data.middleName
+            partyInstance.tin = data.tin
+            partyInstance.save(flush:true)
+
+            personInstance.party = Party.get(partyInstance.id)
+            personInstance.lastName = data.lastName
+            personInstance.firstName = data.firstName
+            personInstance.middleName = data.middleName
+            personInstance.gender = data.gender
+            personInstance.nickname = data.firstName
+            personInstance.personalTitle = data.personalTitle
+            personInstance.maritalStatus = data.maritalStatus
+            personInstance.save(flush:true)
+
+            employeeInstance.party = Party.get(partyInstance.id)
+            employeeInstance.department = data.department
+            employeeInstance.position = data.position
+            employeeInstance.status = 'Active'
+            employeeInstance.save(flush:true)
+
+            partyRole.party = Party.get(partyInstance.id)
+            partyRole.fromDate = new Date()
+            partyRole.status = 'Active'
+            
+            partyRole.role = empRole
+            partyRole.save(flush:true)
+
+            appUtilityService.createContactEntries(data, Party.get(partyInstance.id))
+
+            flash.message = "${message(code: 'default.created.message', args: [message(code: 'employee.label', default: 'Employee'), partyInstance.name])}"
+            redirect(action: "show", id: employeeInstance.id)
+        } else {
+            render(view: "create", model : [employeeData: data])
+            return 
         }
     }
 
     def show = {
-        def customerInstance = Customer.get(params.id)
-        if (!customerInstance) {
-            flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'customer.label', default: 'Customer'), params.id])}"
+        def employeeInstance = Employee.get(params.id)
+
+        def employeeData = appUtilityService.prepareEmployeeData(employeeInstance)
+
+        if (!employeeInstance) {
+            flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'employee.label', default: 'Employee'), params.id])}"
             redirect(action: "list")
         }
         else {
-            [customerInstance: customerInstance]
+            [employeeData: employeeData]
         }
     }
 
     def edit = {
-        def customerInstance = Customer.get(params.id)
-        if (!customerInstance) {
-            flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'customer.label', default: 'Customer'), params.id])}"
+        def employeeInstance = Employee.get(params.id)
+
+        def employeeData = appUtilityService.prepareEmployeeData(employeeInstance)
+
+        if (!employeeData) {
+            flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'employee.label', default: 'Employee'), params.id])}"
             redirect(action: "list")
         }
         else {
-            return [customerInstance: customerInstance]
+            return [employeeData: employeeData]
         }
     }
 
-    def update = {
-        def customerInstance = Customer.get(params.id)
-        if (customerInstance) {
-            if (params.version) {
-                def version = params.version.toLong()
-                if (customerInstance.version > version) {
-                    
-                    customerInstance.errors.rejectValue("version", "default.optimistic.locking.failure", [message(code: 'customer.label', default: 'Customer')] as Object[], "Another user has updated this Customer while you were editing")
-                    render(view: "edit", model: [customerInstance: customerInstance])
-                    return
+    def update = { EmployeeData data ->
+
+        bindData(data, params)
+        data.fullName = data.firstName + ' ' + data.middleName + ' ' + data.lastName
+        
+        def empRole = AppRole.findByRoleCode('CUST')
+        data.roleId = empRole.id
+
+        if(data.validate()){
+
+            def partyInstance = Party.get(data.partyId);
+            partyInstance.name = data.fullName
+            partyInstance.lastName = data.lastName
+            partyInstance.firstName = data.firstName
+            partyInstance.middleName = data.middleName
+            partyInstance.tin = data.tin
+            partyInstance.save(flush:true)
+
+            def personInstance = Person.get(data.personId)
+            personInstance.lastName = data.lastName
+            personInstance.firstName = data.firstName
+            personInstance.middleName = data.middleName
+            personInstance.gender = data.gender
+            personInstance.nickname = data.firstName
+            personInstance.personalTitle = data.personalTitle
+            personInstance.maritalStatus = data.maritalStatus
+            personInstance.save(flush:true)
+
+            def employeeInstance = Employee.get(data.empId)
+            employeeInstance.department = data.department
+            employeeInstance.position = data.position
+            employeeInstance.status = data.status
+            employeeInstance.save(flush:true)
+
+            if(data.status == 'Inactive'){
+                def partyRole = PartyRole.findByParty(partyInstance)
+                if (partyRole != null) {
+                    partyRole.status = 'Inactive'
+                    partyRole.thruDate = new Date()
+                    partyROle.save(flush:true)
                 }
             }
-            customerInstance.properties = params
-            if (!customerInstance.hasErrors() && customerInstance.save(flush: true)) {
-                flash.message = "${message(code: 'default.updated.message', args: [message(code: 'customer.label', default: 'Customer'), customerInstance.id])}"
-                redirect(action: "show", id: customerInstance.id)
-            }
-            else {
-                render(view: "edit", model: [customerInstance: customerInstance])
-            }
-        }
-        else {
-            flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'customer.label', default: 'Customer'), params.id])}"
-            redirect(action: "list")
+            
+            appUtilityService.updateContactEntries(data)
+
+            flash.message = "${message(code: 'default.updated.message', args: [message(code: 'employee.label', default: 'Employee'), partyInstance.name])}"
+            redirect(action: "show", id: employeeInstance.id)
+
+        } else {
+            render(view: "edit", model : [employeeData: data])
+            return 
         }
     }
 
     def delete = {
-        def customerInstance = Customer.get(params.id)
-        if (customerInstance) {
+        def employeeInstance = Employee.get(params.id)
+        if (employeeInstance) {
             try {
-                customerInstance.delete(flush: true)
-                flash.message = "${message(code: 'default.deleted.message', args: [message(code: 'customer.label', default: 'Customer'), params.id])}"
+                employeeInstance.delete(flush: true)
+                flash.message = "${message(code: 'default.deleted.message', args: [message(code: 'employee.label', default: 'Employee'), params.id])}"
                 redirect(action: "list")
             }
             catch (org.springframework.dao.DataIntegrityViolationException e) {
-                flash.message = "${message(code: 'default.not.deleted.message', args: [message(code: 'customer.label', default: 'Customer'), params.id])}"
+                flash.message = "${message(code: 'default.not.deleted.message', args: [message(code: 'employee.label', default: 'Employee'), params.id])}"
                 redirect(action: "show", id: params.id)
             }
         }
         else {
-            flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'customer.label', default: 'Customer'), params.id])}"
+            flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'employee.label', default: 'Employee'), params.id])}"
             redirect(action: "list")
         }
     }
